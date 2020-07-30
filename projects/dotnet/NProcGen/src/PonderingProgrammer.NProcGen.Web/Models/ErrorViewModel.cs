9ab879e600e2491cb7ed288fb93ceea1c7351154
Emil Borowiec <emil@ponderingprogrammer.com>
2020-07-30 15:47:13 +0200

    Initialized webapp for NProcGen

---
+++ projects/dotnet/NProcGen/src/PonderingProgrammer.NProcGen.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PonderingProgrammer.NProcGen.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
