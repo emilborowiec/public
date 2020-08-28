c6ba0b9cdbf0f286cad0ee2cf911716e54c2f2e2
Emil Borowiec <emil@ponderingprogrammer.com>
2020-08-28 20:38:27 +0200

    Initialized softkb

---
+++ projects/java/softkb/src/test/java/net/ponderingprogrammer/softkb/SoftkbApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ponderingprogrammer.softkb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftkbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
