28f4581491e28051806483f14c5e4f40ef55e926
Emil Borowiec <emil@ponderingprogrammer.com>
2020-08-28 14:00:12 +0200

    Initialized bs-expectancy project

---
+++ projects/java/bs-expectancy/src/main/java/net/ponderingprogrammer/bsexpectancy/BsExpectancyApplication.java
@@ -0,0 +1,13 @@
+package net.ponderingprogrammer.bsexpectancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsExpectancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsExpectancyApplication.class, args);
+	}
+
+}
---
+++ projects/java/bs-expectancy/src/test/java/net/ponderingprogrammer/bsexpectancy/BsExpectancyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ponderingprogrammer.bsexpectancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsExpectancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
