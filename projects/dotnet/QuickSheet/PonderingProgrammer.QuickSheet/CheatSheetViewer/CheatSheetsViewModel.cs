0ec6c17b90c7b68c9f035128eb2ab82ff53c67d7
Emil Borowiec <emil@ponderingprogrammer.com>
2020-10-03 15:24:22 +0200

    Automatic adjustment of font size depending on window size and amount of content.

--- projects/dotnet/QuickSheet/PonderingProgrammer.QuickSheet/CheatSheetViewer/CheatSheetsViewModel.cs
+++ projects/dotnet/QuickSheet/PonderingProgrammer.QuickSheet/CheatSheetViewer/CheatSheetsViewModel.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
 using PonderingProgrammer.QuickSheet.Annotations;
+using PonderingProgrammer.QuickSheet.Model;
 using PonderingProgrammer.QuickSheet.Services;
 
 namespace PonderingProgrammer.QuickSheet.CheatSheetViewer
@@ -10,6 +15,7 @@ namespace PonderingProgrammer.QuickSheet.CheatSheetViewer
     {
         private List<CheatSheetViewModel> _cheatSheets;
         private int _currentIndex;
+        private DelegateCommand<string> _updateSizeCommand;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -17,6 +23,14 @@ namespace PonderingProgrammer.QuickSheet.CheatSheetViewer
         public CheatSheetsViewModel()
         {
             ReloadCheatSheets();
+            _updateSizeCommand = new DelegateCommand<string>(
+                s =>
+                {
+                    OnPropertyChanged(nameof(TitleFontSize));
+                    OnPropertyChanged(nameof(SectionFontSize));
+                    OnPropertyChanged(nameof(CaptionFontSize));
+                    OnPropertyChanged(nameof(EntryFontSize));
+                });
         }
         
         public int CurrentIndex
@@ -33,6 +47,8 @@ namespace PonderingProgrammer.QuickSheet.CheatSheetViewer
         
         public CheatSheetViewModel CurrentCheatSheet => CurrentIndex == -1 ? null : _cheatSheets[CurrentIndex];
 
+        public DelegateCommand<string> UpdateSizeCommand => _updateSizeCommand;
+
         private void ReloadCheatSheets()
         {
             _cheatSheets = new List<CheatSheetViewModel>();
@@ -42,6 +58,58 @@ namespace PonderingProgrammer.QuickSheet.CheatSheetViewer
             }
 
             CurrentIndex = _cheatSheets.Count > 0 ? 0 : -1;
+        }
+
+        public int TitleFontSize => (int) (BaseFontSize * 1.5);
+        public int SectionFontSize => (int) (BaseFontSize * 1.2);
+        public int CaptionFontSize => (int) (BaseFontSize * 1.1);
+        public int EntryFontSize => BaseFontSize;
+
+        public int BaseFontSize
+        {
+            get
+            {
+                if (Application.Current.MainWindow != null)
+                {
+                    var h = (int) ((Panel) Application.Current.MainWindow.Content).ActualHeight;
+                    var w = (int) ((Panel) Application.Current.MainWindow.Content).ActualWidth;
+                    return 6 + (h / (GetRequiredLines() * 5));
+                }
+                else
+                {
+                    return 12;
+                }
+            }
+        }
+
+        private int GetRequiredLines()
+        {
+            return Math.Max(GetHighestSectionLineCount(), GetLineCount(CurrentCheatSheet) / GetMaxColumns());
+        }
+
+        private int GetMaxColumns()
+        {
+            return 10;
+        }
+
+        private int GetHighestSectionLineCount()
+        {
+            return CurrentCheatSheet.Sections.Max(GetLineCount);
+        }
+
+        private static int GetLineCount(CheatSheetViewModel cheatSheet)
+        {
+            return 1 + cheatSheet.Sections.Sum(GetLineCount);
+        }
+
+        private static int GetLineCount(SectionViewModel section)
+        {
+            return 1 + section.Cheats.Sum(GetLineCount);
+        }
+
+        private static int GetLineCount(Cheat cheat)
+        {
+            return 1 + cheat.Entries.Count;
         }
 
         [NotifyPropertyChangedInvocator]
---
+++ projects/dotnet/QuickSheet/PonderingProgrammer.QuickSheet/DelegateCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PonderingProgrammer.QuickSheet
+{
+
+    public class DelegateCommand<T> : ICommand where T : class
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+ 
+        public DelegateCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+ 
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+ 
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+ 
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+ 
+        public event EventHandler CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
