53d16740b66bd6197d7080fdb255febd1e8432de
Emil Borowiec <emilborowiec@gmail.com>
2020-12-17 15:16:37 +0100

    Created projects for storing recipes.

---
+++ samples/CSharpSamples/csharp/CSharpLanguageFeatures/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpLanguageFeatures
+{
+    public class Class1
+    {
+        public static void InitializingCollections()
+        {
+            var a = new int[] {1, 2, 3};
+            var l = new List<string>() {"one", "two", "three"};
+            var d = new Dictionary<int, string>()
+            {
+                {1, "one"},
+                {2, "two"},
+                {3, "three"}
+            };
+        }
+    }
+}
